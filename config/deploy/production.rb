cf699ee380ccf50405cac83cfe8ac78ab1035800
chundo <segundo.espana1@gmail.com>
2019-11-25 11:48:04 -0500

    Deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,8 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '206.189.199.36', user: 'root', roles: %w{app db web}
+# server '206.189.199.36', user: 'root', roles: %w{app db web}
+server '06.189.171.69', user: 'root', roles: %w{app db web}
 
 
 
