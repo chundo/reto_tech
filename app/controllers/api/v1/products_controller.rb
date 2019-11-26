55240e6b82a46fc0fb0903423bbf6b47d7b95609
chundo <segundo.espana1@gmail.com>
2019-11-26 10:03:15 -0500

    Hello

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::ProductsController < ApplicationController
     def cart
         user = params[:user]
         @user  = User.find_by(name: user)
+        reto_tech
+chundo
+dominga528
+
         render json: @user
     end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "git@github.com:chundo/reto_tech.git"
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
-set :deploy_to, "/home/chundo/#{fetch :application}"
+set :deploy_to, "/home/powertaxi/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
