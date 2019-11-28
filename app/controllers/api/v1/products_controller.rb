467331301221a689fa034aa5fe88bef2152ee47c
chundo <segundo.espana1@gmail.com>
2019-11-28 00:02:57 -0500

    Orders

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::ProductsController < ApplicationController    
     def cart
         user = params[:user]
-        @user  = User.find_by(name: user)
-        reto_tech
-chundo
-dominga528
-
-        render json: @user
+        @user  = User.find_by(name: user)        
+        orders = @user.orders.where(statu: "PENDIENTE", active: true)
+        if orders.length > 1
+            obj = { messaje: "Orden pendiente de pago", orders: @user.orders.where(statu: "PENDIENTE", active: true) }
+            render json: obj
+        else
+            search = params[:name]
+            products = Product.where("name LIKE ?", "%#{search}%")
+            render json: products
+        end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :city, optional: true
+  has_many :orders
 
   before_create :build_user
   def build_user
